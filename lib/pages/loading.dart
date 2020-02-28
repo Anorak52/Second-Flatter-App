d65b9cbed4f98abeefced54aa9de4b54f94355be
Anorak <45626535+Anorak52@users.noreply.github.com>
2020-02-28 22:01:57 +0300

    time works now
    some update

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'package:world_time/services/world_time.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   @override
@@ -6,10 +9,31 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  String time = 'loading';
+
+  void setupWorldTime() async{
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    print(instance.time);
+    setState(() {
+      time = instance.time;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Loading'),
+      body: Padding(
+        padding: EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
